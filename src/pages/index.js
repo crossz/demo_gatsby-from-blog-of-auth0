08a8f331a8311e3bf006506975aad07776d62afe
crossz <ukcrossa@gmail.com>
2022-03-31 10:01:21 +0800

    :tada: upgrade from gatsby 2 to 4.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
 
-export default () => (
-  <div>
-    <p>Hello Gatsby!</p>
-    <Link to="/account">Go to your account</Link>
-  </div>
-)
+const Index = () => (
+    <div>
+      <p>Hello Gatsby!</p>
+      <Link to="/account">Go to your account</Link>
+    </div>
+  )
+
+export default Index
